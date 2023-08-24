bda3f10cf24cffc4369a25e2270230829de7ccdf
Egor Denisenko <lodpsaf@gmail.com>
2023-08-24 16:08:28 +0300

    Quiz game

---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Egor on 22.08.2023.
+//
+
+import UIKit
+
+class IntoductionViewController: UIViewController {
+
+    @IBAction func unwindToQuizIntoduction(segue: UIStoryboardSegue) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
